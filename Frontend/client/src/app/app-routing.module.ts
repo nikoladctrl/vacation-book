342a3815783f5a73ef2f6cc229a95e51f6dcce23
nikolaradanovic <nikola.radanovic@digitalcontrol.me>
2021-12-09 21:35:18 +0100

    All feature modules are created.

--- Frontend/client/src/app/app-routing.module.ts
+++ Frontend/client/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  { path: 'companies', loadChildren: () => import('./features/companies-feature/companies.module').then(m => m.CompaniesModule) },
+  { path: 'departments', loadChildren: () => import('./features/departments-feature/departments.module').then(m => m.DepartmentsModule) },
+  { path: 'employees', loadChildren: () => import('./features/employees-feature/employees.module').then(m => m.EmployeesModule) },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ Frontend/client/src/app/pages/pages.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PagesRoutingModule } from './pages-routing.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule
+  ],
+  exports: [
+    NotFoundComponent
+  ]
+})
+export class PagesModule { }
