53795ff9d9498a399ea6cc67fa0ee6edbab2663f
nikolaradanovic <nikola.radanovic@digitalcontrol.me>
2021-12-10 22:18:34 +0100

    Views created in each featured module.

--- Frontend/client/src/app/features/companies-feature/companies.module.ts
+++ Frontend/client/src/app/features/companies-feature/companies.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CompaniesRoutingModule } from './companies-routing.module';
+import { CompaniesComponent } from './views/companies/companies.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CompaniesComponent
+  ],
   imports: [
     CommonModule,
     CompaniesRoutingModule
--- Frontend/client/src/app/features/employees-feature/employees.module.ts
+++ Frontend/client/src/app/features/employees-feature/employees.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeesRoutingModule } from './employees-routing.module';
+import { EmployeesComponent } from './views/employees/employees.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EmployeesComponent
+  ],
   imports: [
     CommonModule,
     EmployeesRoutingModule
